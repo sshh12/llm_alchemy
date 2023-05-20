267ff5e4a37c5b8da81472362b61f131599062b0
Shrivu Shankar <shrivu1122@gmail.com>
2023-05-19 17:56:09 -0700

    tweaks

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import "./App.css";
 
 function App() {
   const [elements] = useGet("/get-elements");
+  const size = Math.floor(
+    Math.max(Math.min(window.innerHeight, window.innerWidth) * 0.2, 100)
+  );
   return (
     <div style={{ height: "100%" }}>
       {elements && (
-        <ElementBox starterElements={elements} elementW={100} elementH={100} />
+        <ElementBox
+          starterElements={elements}
+          elementW={size}
+          elementH={size}
+        />
       )}
     </div>
   );
