799d3bb8d43704997df69ff841f77975bfda873e
Shrivu Shankar <shrivu1122@gmail.com>
2023-05-20 13:15:22 -0700

    old code

--- netlify/functions/get-elements.js
+++ netlify/functions/get-elements.js
@@ -3,45 +3,6 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 exports.handler = async (event, context) => {
-  // await prisma.AlchemyRecipesForElements.deleteMany();
-  // await prisma.AlchemyRecipe.deleteMany();
-  // await prisma.AlchemyElement.deleteMany();
-  // await prisma.AlchemyElement.create({
-  //   data: {
-  //     name: "earth",
-  //     imgUrl: "https://i.imgur.com/MVQZAIH.jpeg",
-  //     starterElement: true,
-  //   },
-  // });
-  // await prisma.AlchemyElement.create({
-  //   data: {
-  //     name: "water",
-  //     imgUrl: "https://i.imgur.com/f97Y4iT.jpeg",
-  //     starterElement: true,
-  //   },
-  // });
-  // await prisma.AlchemyElement.create({
-  //   data: {
-  //     name: "air",
-  //     imgUrl: "https://i.imgur.com/Zh8QGKm.jpeg",
-  //     starterElement: true,
-  //   },
-  // });
-  // await prisma.AlchemyElement.create({
-  //   data: {
-  //     name: "fire",
-  //     imgUrl: "https://i.imgur.com/solE9oh.jpeg",
-  //     starterElement: true,
-  //   },
-  // });
-  // await prisma.AlchemyElement.create({
-  //   data: {
-  //     name: "time",
-  //     imgUrl: "https://i.imgur.com/dJyZadY.jpeg",
-  //     starterElement: true,
-  //   },
-  // });
-
   const { starterElements } = event.queryStringParameters;
   let options = {};
   if (starterElements) {
