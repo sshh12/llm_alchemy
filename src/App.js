6205b4b73505c508e233d94c7825b28c7ae39eb5
Shrivu Shankar <shrivu1122@gmail.com>
2023-05-22 16:32:44 -0700

    tweak

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ElementBox from "./components/ElementBox";
 import InfoModal from "./components/InfoModal";
-import { useDefaultPersistentGet, usePollingGet } from "./lib/api";
+import {
+  useDefaultPersistentGet,
+  usePollingGet,
+  useLocalStorage,
+} from "./lib/api";
+import { v4 as uuidv4 } from "uuid";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   const [starterElements, updateStarterElements, resetStarterElements] =
     useDefaultPersistentGet("elements", "/get-elements?starterElements=true");
-  const [stats, pollStats] = usePollingGet("/get-stats");
+  const [userId] = useLocalStorage("alchemy:userId", () => uuidv4());
+  const [stats, pollStats] = usePollingGet(`/get-stats?$userId=${userId}`);
   const size = Math.floor(
     Math.max(Math.min(window.innerHeight, window.innerWidth) * 0.2, 100)
   );
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 
 const BASE_URL = "https://alchemy.sshh.io";
 
@@ -63,4 +63,25 @@ export function useGetFetch() {
       });
   };
   return [fetchValues];
+}
+
+export function useLocalStorage(key, makeDefault) {
+  let [val, _setVal] = useState(null);
+  const setVal = useCallback(
+    (v) => {
+      localStorage.setItem(key, JSON.stringify({ value: v }));
+      _setVal(v);
+    },
+    [key]
+  );
+  const makeDefaultRef = useRef(makeDefault);
+  useEffect(() => {
+    const curVal = localStorage.getItem(key);
+    if (!curVal) {
+      setVal(makeDefaultRef.current());
+    } else {
+      setVal(JSON.parse(curVal).value);
+    }
+  }, [key, setVal, makeDefaultRef]);
+  return [val, setVal];
 }
