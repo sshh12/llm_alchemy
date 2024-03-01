53c127df922fd669661711dc291948ecf259c442
Shrivu Shankar <shrivu1122@gmail.com>
2024-02-29 17:00:17 -0800

    lol dates are weird

--- src/lib/api.js
+++ src/lib/api.js
@@ -88,9 +88,10 @@ export function useLocalStorage(key, makeDefault) {
 }
 
 export function getDate() {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate() + 1).padStart(2, "0");
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = (now.getMonth() + 1).toString().padStart(2, "0");
+  const day = now.getDate().toString().padStart(2, "0");
+
   return `${year}-${month}-${day}`;
 }
